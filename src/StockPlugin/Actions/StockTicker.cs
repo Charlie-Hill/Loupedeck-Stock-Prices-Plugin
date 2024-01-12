9b299d0ca5cff679418e0af0d5e37dbabc8c07f2
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2024-01-12 19:08:47 +0000

    Implementing fix to support multiple tickers/prices in StockTicker command

--- src/StockPlugin/Actions/StockTicker.cs
+++ src/StockPlugin/Actions/StockTicker.cs
@@ -1,107 +1,98 @@
 ﻿namespace Loupedeck.StockPlugin
 {
     using System;
-    using System.Collections.Generic;
+    using System.Collections.Concurrent;
     using System.Globalization;
+    using System.Linq;
     using System.Net.Http;
-    using System.Security.Policy;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
 
     using Newtonsoft.Json;
 
-    // Main Stock Ticker command class.
-
     public class StockTicker : PluginDynamicCommand
     {
         private StockPlugin _plugin;
+        private readonly ConcurrentDictionary<String, dynamic> _tickers;
 
-        private Double _stockPrice = 0;
-        private Double _previousClosePrice = 0;
-        private String _ticker = "";
-
-        // Initializes the command class.
         public StockTicker() : base(displayName: "Stock Ticker", description: "Displays the current stock price", groupName: "")
         {
             this.MakeProfileAction("text;Enter the ticker you want to use");
+            this._tickers = new ConcurrentDictionary<String, dynamic>();
         }
 
         protected override Boolean OnLoad()
         {
             this._plugin = base.Plugin as StockPlugin;
-
-            this._plugin.Tick += (sender, e) => this.ActionImageChanged("");
-
+            this._plugin.Tick += (sender, e) => this.OnPluginTick();
             return base.OnLoad();
         }
 
-        // Method to make the API call to fetch stock info
-        protected async Task<Object> getStockPrice(string ticker)
+        public async void OnPluginTick()
         {
-            using (HttpClient client = new HttpClient())
+            foreach (var ticker in this._tickers.Keys.ToArray())
             {
-                var response = await client.GetAsync("http://api.londonmarket.xyz/stockInfo/" + this._ticker);
+                await this.getStockPrice(ticker);
+            }
+            base.ActionImageChanged();
+        }
+
+        protected async Task<dynamic> getStockPrice(String ticker)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://api.londonmarket.xyz/stockInfo/" + ticker);
                 if (response != null)
                 {
                     var jsonString = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<object>(jsonString);
+                    var tickerData = JsonConvert.DeserializeObject<dynamic>(jsonString);
+                    this._tickers[ticker] = tickerData;
+                    base.ActionImageChanged(ticker);
+                    return tickerData;
                 }
             }
-
             return null;
         }
 
-        // This method is called when the user executes the command.
-        protected override async void RunCommand(String actionParameter)
+        protected override void RunCommand(String actionParameter)
         {
-            this._ticker = actionParameter;
-            dynamic stockResponse = await this.getStockPrice(this._ticker);
-            this._stockPrice = stockResponse.price;
-            this._previousClosePrice = stockResponse.previousClose;
-            //this.ActionImageChanged(); // Notify the Loupedeck service that the command display name and/or image has changed.
-            PluginLog.Info($"{this._ticker} price is {this._stockPrice}");
-            PluginLog.Info($"actionparameter is {actionParameter}");
-            //this._plugin.Tick += (sender, e) => this.ActionImageChanged("");
-            //this.ActionImageChanged(actionParameter);
+            base.ActionImageChanged(actionParameter);
         }
 
         protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
         {
-            CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
-
-            Int32 idx = actionParameter.LastIndexOf("/");
-            String _ticker = actionParameter.Substring(idx + 1).Replace("_", " ");
-
-            this.RunCommand(actionParameter);
-
-            using (var bitmapBuilder = new BitmapBuilder(imageSize))
+            if (!String.IsNullOrEmpty(actionParameter))
             {
-                bitmapBuilder.Clear(BitmapColor.Black);
-                if (!String.IsNullOrEmpty(actionParameter))
+                if (this._tickers.TryGetValue(actionParameter, out dynamic tickerData))
                 {
-                    var x1 = bitmapBuilder.Width * 0.1;
-                    var w = bitmapBuilder.Width * 0.8;
-                    var y1 = bitmapBuilder.Height * 0.42;
-                    var y2 = bitmapBuilder.Height * 0.62;
-                    var y3 = bitmapBuilder.Height * 0.05;
-                    var h = bitmapBuilder.Height * 0.3;
-
-                    BitmapColor color = new BitmapColor();
-                    if (this._stockPrice < this._previousClosePrice)
+                    if (tickerData != null)
                     {
-                        color = new BitmapColor(255, 0, 0);
-                    } else
-                    {
-                        color = new BitmapColor(0, 255, 0);
+                        CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+                        var idx = actionParameter.LastIndexOf("/");
+                        var _ticker = actionParameter.Substring(idx + 1).Replace("_", " ");
+                        using (var bitmapBuilder = new BitmapBuilder(imageSize))
+                        {
+                            bitmapBuilder.Clear(BitmapColor.Black);
+                            var x1 = bitmapBuilder.Width * 0.1;
+                            var w = bitmapBuilder.Width * 0.8;
+                            var y1 = bitmapBuilder.Height * 0.42;
+                            var y2 = bitmapBuilder.Height * 0.62;
+                            var y3 = bitmapBuilder.Height * 0.05;
+                            var h = bitmapBuilder.Height * 0.3;
+                            var color = tickerData.price < tickerData.previousClose ? new BitmapColor(255, 0, 0) : new BitmapColor(0, 255, 0);
+                            bitmapBuilder.DrawText(_ticker, (Int32)x1, (Int32)y3, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 20 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
+                            bitmapBuilder.DrawText(Math.Round((Single)tickerData.price, 2).ToString(), (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 25 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
+                            return bitmapBuilder.ToImage();
+                        }
                     }
-
-                    bitmapBuilder.DrawText(_ticker, (Int32)x1, (Int32)y3, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 20 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
-                    bitmapBuilder.DrawText(this._stockPrice.ToString(), (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 25 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
                 }
-                return bitmapBuilder.ToImage();
+                else
+                {
+                    // Add the ticker to the list of tickers to track its price.
+                    this._tickers[actionParameter] = null;
+                }
             }
+            return base.GetCommandImage(actionParameter, imageSize);
         }
-
     }
-}
+    }
