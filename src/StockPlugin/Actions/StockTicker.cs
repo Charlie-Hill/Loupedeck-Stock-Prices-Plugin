d46e8ea68c2ce6c4aadad7c0c533b3e166f97bd7
Charlie <30829462+Charlie-Hill@users.noreply.github.com>
2024-01-15 00:42:33 +0000

    StockTicker colored background, white text option

--- src/StockPlugin/Actions/StockTicker.cs
+++ src/StockPlugin/Actions/StockTicker.cs
@@ -91,16 +91,18 @@
                         var _ticker = actionParameter.Substring(idx + 1).Replace("_", " ");
                         using (var bitmapBuilder = new BitmapBuilder(imageSize))
                         {
-                            bitmapBuilder.Clear(BitmapColor.Black);
+                            var backgroundColor = (tickerData.price < tickerData.previousClose) ? new BitmapColor(255, 0, 0) : new BitmapColor(8, 170, 63);
+                            //bitmapBuilder.Clear(new BitmapColor(0, 255, 0));
+                            bitmapBuilder.Clear(backgroundColor);
                             var x1 = bitmapBuilder.Width * 0.1;
                             var w = bitmapBuilder.Width * 0.8;
                             var y1 = bitmapBuilder.Height * 0.42;
                             var y2 = bitmapBuilder.Height * 0.62;
                             var y3 = bitmapBuilder.Height * 0.05;
                             var h = bitmapBuilder.Height * 0.3;
-                            var color = tickerData.price < tickerData.previousClose ? new BitmapColor(255, 0, 0) : new BitmapColor(0, 255, 0);
-                            bitmapBuilder.DrawText(_ticker, (Int32)x1, (Int32)y3, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 20 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
-                            bitmapBuilder.DrawText(Math.Round((Single)tickerData.price, 2).ToString(), (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, color, imageSize == PluginImageSize.Width90 ? 25 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
+                            //var color = tickerData.price < tickerData.previousClose ? new BitmapColor(255, 0, 0) : new BitmapColor(0, 255, 0);
+                            bitmapBuilder.DrawText(_ticker, (Int32)x1, (Int32)y3, (Int32)w, (Int32)h, BitmapColor.White, imageSize == PluginImageSize.Width90 ? 20 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
+                            bitmapBuilder.DrawText(Math.Round((Single)tickerData.price, 2).ToString(), (Int32)x1, (Int32)y2, (Int32)w, (Int32)h, BitmapColor.White, imageSize == PluginImageSize.Width90 ? 25 : 9, imageSize == PluginImageSize.Width90 ? 2 : 0, 10);
                             return bitmapBuilder.ToImage();
                         }
                     }
